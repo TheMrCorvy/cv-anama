4aaf591b61e203181098ad13845d77eb629271f3
Gonzalo Corvalan <salvatore.hakase@gmail.com>
2026-02-02 17:59:20 -0300

    added layout for demo page

---
+++ app/demo/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from 'next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from '@vercel/analytics/react';
+import { Geist, Geist_Mono } from 'next/font/google';
+import '../globals.css';
+
+const geistSans = Geist({
+    variable: '--font-geist-sans',
+    subsets: ['latin'],
+});
+
+const geistMono = Geist_Mono({
+    variable: '--font-geist-mono',
+    subsets: ['latin'],
+});
+
+export const metadata: Metadata = {
+    title: 'Ghibli-Style Shadcn/ui',
+    description: 'A ghibli-style shadcn/ui theme for next.js projects',
+};
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="en">
+            <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+                {children}
+                <SpeedInsights />
+                <Analytics />
+            </body>
+        </html>
+    );
+}
