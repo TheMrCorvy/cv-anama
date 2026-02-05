eb5b029df13313ecc3bc2f53d2f01c20d076a5c0
Gonzalo Corvalan <salvatore.hakase@gmail.com>
2026-02-05 13:02:01 -0300

    finished contact section

--- components/sections/Contact/index.tsx
+++ components/sections/Contact/index.tsx
@@ -1,9 +1,9 @@
-import { FC } from "react";
+import { FC } from 'react';
 
 import { Card, CardContent, CardFooter, CardHeader } from '@/components/ui/card';
 import { FaWhatsapp, FaPhone, FaEnvelope, FaLinkedin, FaMapMarkerAlt } from 'react-icons/fa';
 import { Separator } from '@/components/ui/separator';
-import { Button } from "@/components/ui/button";
+import { Button } from '@/components/ui/button';
 
 const Contact: FC = () => {
     return (
@@ -11,7 +11,10 @@ const Contact: FC = () => {
             className="mt-12 mb-12 flex flex-col gap-5 w-full justify-center px-6"
             id="contact-info"
         >
-            <Card element='section' className="mx-auto w-full sm:w-2/3 md:w-1/2 lg:w-1/2 xl:w-1/2 h-auto">
+            <Card
+                element="section"
+                className="mx-auto w-full sm:w-2/3 md:w-1/2 lg:w-1/2 xl:w-1/2 h-auto"
+            >
                 <CardHeader className="pb-2">
                     <div className="flex justify-center items-center gap-4">
                         <div className="text-center mt-4">
@@ -19,50 +22,66 @@ const Contact: FC = () => {
                         </div>
                     </div>
                 </CardHeader>
-                <CardContent className="space-y-4">
+                <div className="w-full px-6">
                     <Separator />
-                    <p className="text-xl flex items-center gap-2">
-                        <FaWhatsapp />
-                        <a
-                            href="https://api.whatsapp.com/send?phone=+5491150488031&text=Hola!%20Me%20interesa%20tu%20CV!"
-                            rel="noreferrer"
-                            target="_blank"
-                        >
-                            <Button variant="link" size="lg" className="cursor-pointer">
-                                +54 9 11 5048-8031
-                            </Button>
-                        </a>
-                    </p>
-                    <p className="text-xl flex items-center gap-2">
-                        <FaPhone />
-                        <a href="tel:+5491176059251" target="_blank">
-                            <Button variant="link" size="lg" className="cursor-pointer">
-                                +54 9 11 5048-8031
-                            </Button>
-                        </a>
-                    </p>
-                    <p className="text-xl flex items-center gap-2">
-                        <FaEnvelope />
-                        <a href="mailto:yesicaanama2@gmail.com" target="_blank" rel="noreferrer">
-                            <Button variant="link" size="lg" className="cursor-pointer">
-                                yesicaanama2@gmail.com
-                            </Button>
-                        </a>
-                    </p>
-                    <p className="text-xl flex items-center gap-2">
-                        <FaLinkedin />
-                        <a
-                            href="https://www.linkedin.com/in/yesica-anama-rodas"
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            <Button variant="link" size="lg" className="cursor-pointer">
-                                yesica-anama-rodas
-                            </Button>
-                        </a>
-                    </p>
-                    <div className="flex items-center gap-2">
-                        <FaMapMarkerAlt />
+                </div>
+                <CardContent>
+                    <div className="space-y-4 grid grid-cols-1 md:grid-cols-2 w-full md:w-2/3 mx-auto">
+                        <div className="flex flex-row items-center gap-2 mb-2 md:mb-6">
+                            <FaWhatsapp size={24} />
+                            <div>
+                                <a
+                                    href="https://api.whatsapp.com/send?phone=+5491150488031&text=Hola!%20Me%20interesa%20tu%20CV!"
+                                    rel="noreferrer"
+                                    target="_blank"
+                                >
+                                    <Button variant="link" size="lg" className="cursor-pointer">
+                                        +54 9 11 5048-8031
+                                    </Button>
+                                </a>
+                            </div>
+                        </div>
+                        <div className="flex items-center gap-2 text-center mb-2 md:mb-6">
+                            <FaPhone size={24} />
+                            <div>
+                                <a href="tel:+5491176059251" target="_blank">
+                                    <Button variant="link" size="lg" className="cursor-pointer">
+                                        +54 9 11 5048-8031
+                                    </Button>
+                                </a>
+                            </div>
+                        </div>
+                        <div className="flex items-center gap-2 text-center mb-2 md:mb-6">
+                            <FaEnvelope size={24} />
+                            <div>
+                                <a
+                                    href="mailto:yesicaanama2@gmail.com"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    <Button variant="link" size="lg" className="cursor-pointer">
+                                        yesicaanama2@gmail.com
+                                    </Button>
+                                </a>
+                            </div>
+                        </div>
+                        <div className="flex items-center gap-2 text-center mb-2 md:mb-6">
+                            <FaLinkedin size={24} />
+                            <div>
+                                <a
+                                    href="https://www.linkedin.com/in/yesica-anama-rodas"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    <Button variant="link" size="lg" className="cursor-pointer">
+                                        /in/yesica-anama-rodas
+                                    </Button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex justify-center items-center gap-2 text-left md:text-center mb-2">
+                        <FaMapMarkerAlt size={24} />
                         <div>
                             <p className="text-md mb-0">
                                 Matheu 3157, Mar Del Plata, Buenos Aires.
@@ -80,6 +99,6 @@ const Contact: FC = () => {
             </Card>
         </article>
     );
-}
+};
 
-export default Contact
+export default Contact;
