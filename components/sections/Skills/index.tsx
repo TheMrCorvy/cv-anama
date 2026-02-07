786df9be6d5e6a723c6da8516501da3b53734b60
Gonzalo Salvador Corvalán <salvatore.hakase@gmail.com>
2026-02-07 10:39:57 +0000

    added accordion component

--- components/sections/Skills/index.tsx
+++ components/sections/Skills/index.tsx
@@ -1,30 +1,73 @@
-import { FC } from "react";
+import { FC } from 'react';
 
 import { Card, CardContent, CardHeader } from '@/components/ui/card';
 import { Separator } from '@/components/ui/separator';
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+} from '@/components/ui/accordion';
 
 const Skills: FC = () => {
-return (
-    <Card element="section" className="mx-auto w-full sm:w-2/3 md:w-1/2 lg:w-1/2 xl:w-1/2 h-auto">
-        <CardHeader className="pb-2">
-            <div className="flex justify-center items-center gap-4">
-                <div className="text-center mt-4">
-                    <h2 className="text-4xl font-bold">Aptitudes</h2>
+    return (
+        <Card
+            element="section"
+            className="mx-auto w-full sm:w-2/3 md:w-1/2 lg:w-1/2 xl:w-1/2 h-auto"
+        >
+            <CardHeader className="pb-2">
+                <div className="flex justify-center items-center gap-4">
+                    <div className="text-center mt-4">
+                        <h2 className="text-4xl font-bold">Aptitudes</h2>
+                    </div>
                 </div>
-            </div>
-        </CardHeader>
-        <CardContent className="space-y-4">
-            <Separator />
-            <ul className="list-disc list-inside">
-                <li>Tratamientos del rostro y piel.</li>
-                <li>Uso de cremas, y herramientas de belleza.</li>
-                <li>Estudio y desarrollo de nuevas habilidades.</li>
-                <li>Capacidad de adaptabilidad a distintos entornos.</li>
-                <li>Entendimiento de la teoría al momento de fundamentar un tratamiento.</li>
-            </ul>
-        </CardContent>
-    </Card>
-);
-}
+            </CardHeader>
+            <CardContent className="space-y-4">
+                <Separator />
+                <Accordion type="single" collapsible className="w-full">
+                    <AccordionItem value="skills-item-1">
+                        <AccordionTrigger>Tratamientos del rostro y piel.</AccordionTrigger>
+                        <AccordionContent>
+                            Yes. It adheres to the WAI-ARIA design pattern.
+                        </AccordionContent>
+                    </AccordionItem>
+                    <AccordionItem value="skills-item-2">
+                        <AccordionTrigger>
+                            Uso de cremas, y herramientas de belleza.
+                        </AccordionTrigger>
+                        <AccordionContent>
+                            Yes. It comes with default styles that matches the other components
+                            aesthetic.
+                        </AccordionContent>
+                    </AccordionItem>
+                    <AccordionItem value="skills-item-3">
+                        <AccordionTrigger>
+                            Estudio y desarrollo de nuevas habilidades.
+                        </AccordionTrigger>
+                        <AccordionContent>
+                            Yes. It is animated by default, but you can disable it if you prefer.
+                        </AccordionContent>
+                    </AccordionItem>
+                    <AccordionItem value="skills-item-4">
+                        <AccordionTrigger>
+                            Capacidad de adaptabilidad a distintos entornos.
+                        </AccordionTrigger>
+                        <AccordionContent>
+                            Yes. It is animated by default, but you can disable it if you prefer.
+                        </AccordionContent>
+                    </AccordionItem>
+                    <AccordionItem value="skills-item-5">
+                        <AccordionTrigger>
+                            Entendimiento de la teoría al momento de fundamentar un tratamiento.
+                        </AccordionTrigger>
+                        <AccordionContent>
+                            Yes. It is animated by default, but you can disable it if you prefer.
+                        </AccordionContent>
+                    </AccordionItem>
+                </Accordion>
+            </CardContent>
+        </Card>
+    );
+};
 
-export default Skills
+export default Skills;
