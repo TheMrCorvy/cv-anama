dd83976fa932bb767d4752b8490b0f2f561a9f68
Gonzalo Salvador Corvalán <salvatore.hakase@gmail.com>
2026-02-07 10:39:45 +0000

    added accordion component

--- components/sections/Achievements/index.tsx
+++ components/sections/Achievements/index.tsx
@@ -1,27 +1,58 @@
-import { Card, CardContent, CardHeader } from "@/components/ui/card";
-import { Separator } from "@/components/ui/separator";
-import { FC } from "react";
+import { FC } from 'react';
+import { Card, CardContent, CardHeader } from '@/components/ui/card';
+import { Separator } from '@/components/ui/separator';
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+} from '@/components/ui/accordion';
 
 const Achievements: FC = () => {
-return (
-    <Card element='section' className="mx-auto w-full sm:w-2/3 md:w-1/2 lg:w-1/2 xl:w-1/2 h-auto">
-        <CardHeader className="pb-2">
-            <div className="flex justify-center items-center gap-4">
-                <div className="text-center mt-4">
-                    <h2 className="text-4xl font-bold">Logros</h2>
+    return (
+        <Card
+            element="section"
+            className="mx-auto w-full sm:w-2/3 md:w-1/2 lg:w-1/2 xl:w-1/2 h-auto"
+        >
+            <CardHeader className="pb-2">
+                <div className="flex justify-center items-center gap-4">
+                    <div className="text-center mt-4">
+                        <h2 className="text-4xl font-bold">Logros</h2>
+                    </div>
                 </div>
-            </div>
-        </CardHeader>
-        <CardContent className="space-y-4">
-            <Separator />
-            <ul className="list-disc list-inside">
-                <li>Diplomatura en Cosmetología con nota promedio de 8.</li>
-                <li>Diplomatura en Cosmiatría con nota promedio de 10.</li>
-                <li>Buenos resultados en prácticas con pacientes reales.</li>
-            </ul>
-        </CardContent>
-    </Card>
-);
-}
+            </CardHeader>
+            <CardContent className="space-y-4">
+                <Separator />
+                <Accordion type="single" collapsible className="w-full">
+                    <AccordionItem value="achievements-item-1">
+                        <AccordionTrigger>
+                            Diplomatura en Cosmetología con nota promedio de 8.
+                        </AccordionTrigger>
+                        <AccordionContent>
+                            Yes. It adheres to the WAI-ARIA design pattern.
+                        </AccordionContent>
+                    </AccordionItem>
+                    <AccordionItem value="achievements-item-2">
+                        <AccordionTrigger>
+                            Diplomatura en Cosmiatría con nota promedio de 10.
+                        </AccordionTrigger>
+                        <AccordionContent>
+                            Yes. It comes with default styles that matches the other components
+                            aesthetic.
+                        </AccordionContent>
+                    </AccordionItem>
+                    <AccordionItem value="achievements-item-3">
+                        <AccordionTrigger>
+                            Buenos resultados en prácticas con pacientes reales.
+                        </AccordionTrigger>
+                        <AccordionContent>
+                            Yes. It is animated by default, but you can disable it if you prefer.
+                        </AccordionContent>
+                    </AccordionItem>
+                </Accordion>
+            </CardContent>
+        </Card>
+    );
+};
 
-export default Achievements
+export default Achievements;
